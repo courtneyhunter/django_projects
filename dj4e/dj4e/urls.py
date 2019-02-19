98ff9a945e474fe49b13d66a98425bc1976498f2
Courtney Hunter <courthun@umich.edu>
2019-02-19 12:48:52 -0500

    remove dj4e/urls.py lines

--- dj4e/dj4e/urls.py
+++ dj4e/dj4e/urls.py
@@ -20,7 +20,7 @@ from django.urls import include
 
 urlpatterns = [
     path('', include('home.urls')),
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('autos/', include('autos.urls')),
+#    path('admin/', admin.site.urls),
+#    path('accounts/', include('django.contrib.auth.urls')),
+#    path('autos/', include('autos.urls')),
 ]
