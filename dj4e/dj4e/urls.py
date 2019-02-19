7bc67cc694ff8b2c4c9fb75c5ade5eb8797c5f94
Courtney Hunter <courthun@umich.edu>
2019-02-19 15:41:41 -0500

    working hello world

--- dj4e/dj4e/urls.py
+++ dj4e/dj4e/urls.py
@@ -14,13 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+# from django.contrib import admin
+# from django.urls import path
+# from django.urls import include
+#
+# urlpatterns = [
+#     path('', include('home.urls')),
+# #    path('admin/', admin.site.urls),
+# #    path('accounts/', include('django.contrib.auth.urls')),
+# #    path('autos/', include('autos.urls')),
+# ]
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include('home.urls')),
-#    path('admin/', admin.site.urls),
-#    path('accounts/', include('django.contrib.auth.urls')),
-#    path('autos/', include('autos.urls')),
 ]
--- dj4e/home/urls.py
+++ dj4e/home/urls.py
@@ -3,6 +3,14 @@ from . import views
 from django.views.generic import TemplateView
 
 # https://docs.djangoproject.com/en/2.1/topics/http/urls/
+# urlpatterns = [
+#     path('', TemplateView.as_view(template_name='home2.html'), name='home'),
+#     path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
+# ]
+
+
+#test
+# https://docs.djangoproject.com/en/2.1/topics/http/urls/
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('hello/', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
